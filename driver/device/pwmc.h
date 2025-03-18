540a1fd805ec60b4e856e8be6fb6ce3e79a51f9d
FlyyingPiggy2020 <154562451@qq.com>
2025-03-18 09:11:48 +0800

    refactor:重构pwmc驱动。设计为仅支持单个通道，但是可以同时调整pwm频率和占空比

--- driver/device/pwmc.h
+++ driver/device/pwmc.h
@@ -4,15 +4,16 @@
  * @Author       : lxf
  * @Date         : 2024-12-05 14:37:50
  * @LastEditors  : FlyyingPiggy2020 154562451@qq.com
- * @LastEditTime : 2025-03-12 10:52:48
+ * @LastEditTime : 2025-03-17 09:22:47
  * @Brief        :
+ * ATTENTION: THIS DRIVER IS DESIGNED FOR ONE CHANNEL ONLY.
  * [-] this driver is control device pwm wave.user should implement follow function:
  *   (1) init, deinit , enable, updatre_duty_raw, get_update_duty_raw.
  *
  * [-] How to use:
- *   other args data type is pwmc_ioctl_param.
- *   args of IOCTL_PWMC_ENABLE_CHANNEL is channel name. for example channel name in stm32
- *   is TIM_CHANNEL_1.
+ *   args data type is pwmc_ioctl_param.
+ *   (1) device_open
+ *   (2) Use IOCTL_PWMC_ENABLE_CHANNEL to enable channel.
  */
 
 #ifndef __PWMC_H__
@@ -32,46 +33,48 @@ extern "C" {
 #define IOCTL_PWMC_DISABLE               (IOCTL_USER_START + 0x01)
 #define IOCTL_PWMC_GET_FREQ              (IOCTL_USER_START + 0x02)
 #define IOCTL_PWMC_SET_FREQ              (IOCTL_USER_START + 0x03)
-#define IOCTL_PWMC_GET_NUMBER_OF_CHANNEL (IOCTL_USER_START + 0x04)
 #define IOCTL_PWMC_GET_DUTY              (IOCTL_USER_START + 0x05)
 #define IOCTL_PWMC_SET_DUTY              (IOCTL_USER_START + 0x06)
 #define IOCTL_PWMC_GET_DUTY_RAW          (IOCTL_USER_START + 0x07)
 #define IOCTL_PWMC_SET_DUTY_RAW          (IOCTL_USER_START + 0x08)
-#define IOCTL_PWMC_ENABLE_CHANNEL        (IOCTL_USER_START + 0x09)
-#define IOCTL_PWMC_DISABLE_CHANNEL       (IOCTL_USER_START + 0x0A)
-#define IOCTL_PWMC_SET_IRQ_HANDLER       (IOCTL_USER_START + 0x0B)
-#define IOCTL_PWMC_GET_DUTY_RAW_MAX      (IOCTL_USER_START + 0x0C)
+#define IOCTL_PWMC_SET_IRQ_HANDLER       (IOCTL_USER_START + 0x09)
+#define IOCTL_PWMC_SET_FREQ_DUTY         (IOCTL_USER_START + 0x0A)
 /*---------- type define ----------*/
 typedef int32_t (*pwmc_irq_handler_fn)(uint32_t irq_handler, void *args, uint32_t len);
 
+// this struct is describe one channel
 typedef struct {
-    uint32_t freq;
-    uint32_t number_of_channel;
-    uint32_t raw_max;
+    bool is_enable; //true:enable; false:disable
+    uint32_t clock;//timer clock. unit:hz
+    uint32_t frequence;//timer frequence
+    float duty;//duty per unit
+    struct {
+        uint32_t prescaler;
+        uint32_t arr;
+        uint32_t crr;
+    } priv;
     struct {
         bool (*init)(void);
         void (*deinit)(void);
         bool (*enable)(bool ctrl);
         /**
-         * @brief update timer pwm ccr value
-         * @param {uint32_t} channel: timer channel
-         * @param {uint32_t} raw : timer ccr value
+         * @brief update frequence by precaler and arr
+         * @param {uint32_t} precaler
+         * @param {uint32_t} arr
          * @return {*}
          */
-        bool (*update_duty_raw)(uint32_t channel, uint32_t raw);
+        int32_t (*update_frequence_arr)(uint32_t precaler, uint32_t arr);
+        /**
+         * @brief update timer pwm ccr value. if crr equal to zero or arr, you should let gpio output direct.
+         * @param {uint32_t} crr : timer ccr value
+         * @return {*}
+         */
+        int32_t (*update_duty_crr)(uint32_t raw);
         /**
          * @brief get timer ccr value
-         * @param {uint32_t} channel : timer channel
          * @return {*}
          */
-        uint32_t (*get_duty_raw)(uint32_t channel);
-        /**
-         * @brief enable or disable timer channel
-         * @param {uint32_t} channel :timer channel
-         * @param {bool} ctrl : ture-enable; false-disable
-         * @return {*}
-         */
-        bool (*channel_ctrl)(uint32_t channel, bool ctrl);
+        int32_t (*get_duty_crr)(void);
         /**
          * @brief private date(callback function of irqhandler)
          * @return {*}
@@ -80,22 +83,10 @@ typedef struct {
     } ops;
 } pwmc_describe_t;
 
-union pwmc_ioctl_param {
-    struct {
-        uint32_t freq;
-    } freq;
-    struct {
-        uint32_t channel;
-    } number_of_channel;
-
-    struct {
-        uint32_t channel;
-        uint32_t raw; // ccr value.
-    } duty_raw;
-    struct {
-        uint32_t channel;
-        float duty; // duty per unit.取值范围[0,1];
-    } duty;
+struct pwmc_ioctl_param {
+    uint32_t freq;
+    uint32_t crr; // ccr value.
+    float duty; // duty per unit.取值范围[0,1];
 };
 
 struct pwmc_irq_param {
