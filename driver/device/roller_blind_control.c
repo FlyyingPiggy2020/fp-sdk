907ae475fec684e612033c14a3ca22c7f9472792
Lu Xianfan <62139687+FlyyingPiggy2020@users.noreply.github.com>
2025-05-20 14:25:14 +0800

    增加窗帘控制的业务逻辑

---
+++ driver/device/roller_blind_control.c
@@ -0,0 +1,291 @@
+/*
+ * Copyright (c) 2025 by Lu Xianfan.
+ * @FilePath     : roller_blind_control.c
+ * @Author       : lxf
+ * @Date         : 2025-05-14 15:36:33
+ * @LastEditors  : lxf_zjnb@qq.com
+ * @LastEditTime : 2025-05-20 10:06:20
+ * @Brief        : 卷帘,百叶帘的控制业务逻辑驱动程序
+ */
+
+/*---------- includes ----------*/
+#include "roller_blind_control.h"
+#include "driver.h"
+#include "drv_err.h"
+#include "fpevent.h"
+
+/*---------- macro ----------*/
+/*---------- type define ----------*/
+/*---------- variable prototype ----------*/
+/*---------- function prototype ----------*/
+static int32_t _motor_open(driver_t **pdrv);
+static int32_t _motor_close(driver_t **pdrv);
+static int32_t _motor_ioctl(driver_t **pdrv, uint32_t cmd, void *args);
+static int32_t _motor_irq_handler(driver_t **pdrv, uint32_t irq_handler, void *args, uint32_t length);
+
+//api
+static int32_t _motor_stop(roller_blind_control_describe_t *pdesc, void *args);
+
+//priv
+static int32_t __motor_run_inc(roller_blind_control_describe_t *pdesc, void *args);
+static int32_t __motor_stop_without_logic(roller_blind_control_describe_t *pdesc);
+static int32_t __motor_run_dec(roller_blind_control_describe_t *pdesc, void *args);
+/*---------- variable ----------*/
+DRIVER_DEFINED(rb_motorc, _motor_open, _motor_close, NULL, NULL, _motor_ioctl, _motor_irq_handler);
+
+static struct protocol_callback ioctl_cbs[] = {
+    { IOCTL_MOTORC_OPEN, NULL },
+    { IOCTL_MOTORC_CLOSE, NULL },
+    { IOCTL_MOTORC_STOP, _motor_stop },
+    { IOCTL_MOTORC_GOTO_ROUTE, NULL },
+    { IOCTL_MOTORC_SET_UP_ROUTE, NULL },
+    { IOCTL_MOTORC_SET_DOWN_ROUTE, NULL },
+    { IOCTL_MOTORC_CLEAR_ROUTE, NULL },
+    { IOCTL_MOTORC_TURN, NULL },
+};
+/*---------- function ----------*/
+
+/**
+ * @brief 返回两个值之间的绝对差值
+ * @param {int32} v1
+ * @param {int32} v2
+ * @return {*}
+ */
+static uint32_t abs32(int32_t v1, int32_t v2)
+{
+    int32_t v = v1;
+    v -= v2;
+    if (v < 0)
+        v = 0 - v;
+    return (v);
+}
+
+static int32_t _motor_open(driver_t **pdrv)
+{
+    roller_blind_control_describe_t *pdesc = NULL;
+    int32_t err = DRV_ERR_WRONG_ARGS;
+
+    ASSERT(pdrv);
+    pdesc = container_of(pdrv, device_t, pdrv)->pdesc;
+    do {
+        if (!pdesc) {
+            break;
+        }
+
+        if (!pdesc->ops.save_config || !pdesc->ops.load_config) {
+            break;
+        }
+
+        // 读取失败,重新初始化
+        if (pdesc->ops.load_config() == false) {
+            pdesc->config.route_curr = 0;
+            pdesc->config.route_down = MOTOR_ROUTE_FREE;
+            pdesc->config.route_up = MOTOR_ROUTE_FREE;
+            pdesc->ops.save_config();
+        } 
+
+        //设置默认参数
+        pdesc->param.space_min = MOTOR_SPACE_MIN;
+        pdesc->param.time_stop_delay = MOTOR_SWITCH_TIME;
+        if (pdesc->ops.init) {
+            pdesc->ops.init();
+        }
+    } while (0);
+
+    return err;
+}
+static int32_t _motor_close(driver_t **pdrv)
+{
+    roller_blind_control_describe_t *pdesc = NULL;
+    int32_t err = DRV_ERR_WRONG_ARGS;
+
+    ASSERT(pdrv);
+    pdesc = container_of(pdrv, device_t, pdrv)->pdesc;
+    do {
+        if (!pdesc) {
+            break;
+        }
+
+        if (pdesc->ops.deinit) {
+            pdesc->ops.deinit();
+        }
+    } while (0);
+
+    return err;
+}
+static int32_t _motor_ioctl(driver_t **pdrv, uint32_t cmd, void *args)
+{
+    roller_blind_control_describe_t *pdesc = NULL;
+    int32_t err = DRV_ERR_WRONG_ARGS;
+    int32_t (*cb)(roller_blind_control_describe_t *, void *) = NULL;
+    
+    ASSERT(pdrv);
+    pdesc = container_of(pdrv, device_t, pdrv)->pdesc;
+    do {
+        if (!pdesc) {
+            break;
+        }
+        cb = (int32_t(*)(roller_blind_control_describe_t *, void *))protocol_callback_find(cmd, ioctl_cbs, ARRAY_SIZE(ioctl_cbs));
+        if (!cb) {
+            break;
+        }
+        err = cb(pdesc, args);
+    } while (0);
+
+    return err;
+}
+
+
+static int32_t _motor_irq_handler(driver_t **pdrv, uint32_t irq_handler, void *args, uint32_t length)
+{
+    roller_blind_control_describe_t *pdesc = NULL;
+    int32_t err = DRV_ERR_WRONG_ARGS;
+
+    ASSERT(pdrv);
+    pdesc = container_of(pdrv, device_t, pdrv)->pdesc;
+    do {
+        if (!pdesc) {
+            break;
+        }
+        do {
+            
+        } while (0);
+        
+    } while (0);
+
+    return err;
+}
+
+/**
+ * @brief 电机停止,但是逻辑上不停止(用于正反转切换时，不立即切换，而是先停止这个逻辑)
+ * @param {roller_blind_control_describe_t} *pdesc
+ * @param {void} *args
+ * @return {*}
+ */
+static int32_t __motor_stop_without_logic(roller_blind_control_describe_t *pdesc)
+{
+    if (pdesc->ops.motor_stop) {
+        pdesc->ops.motor_stop();
+    }
+
+    if (pdesc->priv.state.is_running != MSTATE_STOP) {
+        pdesc->priv.state.is_running = MSTATE_STOP;
+    }
+}
+
+/**
+ * @brief 电机正转
+ * @param {roller_blind_control_describe_t} *pdesc
+ * @param {void} *args
+ * @return {*}
+ */
+static int32_t __motor_run_inc(roller_blind_control_describe_t *pdesc, void *args)
+{
+    if (pdesc->priv.state.is_running != MSTATE_RUN_INC) {
+        pdesc->ops.motor_inc();
+    }
+    pdesc->priv.state.is_running = MSTATE_RUN_INC;
+}
+
+/**
+ * @brief 电机反转
+ * @param {roller_blind_control_describe_t} *pdesc
+ * @param {void} *args
+ * @return {*}
+ */
+static int32_t __motor_run_dec(roller_blind_control_describe_t *pdesc, void *args)
+{
+    if (pdesc->priv.state.is_running != MSTATE_RUN_DEC) {
+        pdesc->ops.motor_dec();
+    }
+    pdesc->priv.state.is_running = MSTATE_RUN_DEC;
+}
+
+static bool __motor_goto_route(roller_blind_control_describe_t *pdesc, int32_t route)
+{
+    bool ret = false;
+    do {
+        if (route == MOTOR_ROUTE_FREE) {
+            break;
+        }
+
+        if (abs32(pdesc->config.route_curr, route) <= pdesc->param.space_min) {
+            break;
+        }
+
+        if (!pdesc->ops.motor_stop) {
+            break;
+        }
+
+        pdesc->priv.control.target_route = route;
+        if (pdesc->priv.control.target_route > pdesc->config.route_curr) {
+            if (pdesc->priv.state.state_curr == MSTATE_RUN_DEC) {
+                pdesc->priv.control.time_stop_delay = pdesc->param.time_stop_delay;
+                __motor_stop_without_logic(pdesc);
+            }
+            
+        } else if (pdesc->priv.control.target_route < pdesc->config.route_curr) {
+            
+        }
+    }while(0);
+    
+    return ret;
+}
+
+/**
+ * @brief 电机停止-api
+ * @param {roller_blind_control_describe_t} *pdesc
+ * @param {void} *args
+ * @return {*}
+ */
+static int32_t _motor_stop(roller_blind_control_describe_t *pdesc, void *args)
+{
+    int32_t err = DRV_ERR_ERROR;
+    
+    do {
+        pdesc->priv.state.state_curr = MSTATE_STOP;
+        pdesc->priv.control.target_route = MOTOR_ROUTE_FREE;
+
+        if (!pdesc->ops.motor_stop) {
+            err = DRV_ERR_POINT_NONE;
+            break;
+        }
+
+        pdesc->ops.motor_stop();
+
+        if (pdesc->priv.state.is_running != MSTATE_STOP) {
+            pdesc->priv.state.is_running = MSTATE_STOP;
+        }
+
+        err = DRV_ERR_OK;
+    } while(0);
+
+    return err;
+}
+
+/**
+ * @brief 电机上行-api
+ * @param {roller_blind_control_describe_t} *pdesc
+ * @param {void} *args
+ * @return {*}
+ */
+static int32_t _motor_to_open_up(roller_blind_control_describe_t *pdesc, void *args)
+{
+    // 同方向遇阻不再运行
+    uint8_t mode = (uint8_t *)args;
+
+    if (EVENT_IS_AND_BIT_SET(pdesc->priv.flag.state, MFLAG_RESISTANCE_INC)) {
+        return DRV_ERR_ERROR;
+    }
+    
+    if ((pdesc->config.route_up == MOTOR_ROUTE_FREE) || (mode & MMODE_OVERROUTE)) {
+        if ((mode & 0x0f) & MMODE_SSTEP) {
+            
+        }
+    }
+}
+static int32_t _motor_to_close_down(roller_blind_control_describe_t *pdesc, void *args)
+{
+    
+}
+/*---------- end of file ----------*/
---
+++ driver/device/roller_blind_control.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2025 by Lu Xianfan.
+ * @FilePath     : roller_blind_control.h
+ * @Author       : lxf
+ * @Date         : 2025-05-14 15:36:45
+ * @LastEditors  : lxf_zjnb@qq.com
+ * @LastEditTime : 2025-05-20 08:32:31
+ * @Brief        : 卷帘，百叶帘的驱动
+ */
+
+#ifndef __ROLLER_BLIND_CONTROL_H__
+#define __ROLLER_BLIND_CONTROL_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*---------- includes ----------*/
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include "device.h"
+/*---------- macro ----------*/
+#define IOCTL_MOTORC_OPEN           (IOCTL_USER_START + 0x00) // 开(点动，连续)
+#define IOCTL_MOTORC_CLOSE          (IOCTL_USER_START + 0x01) // 关(点动，连续)
+#define IOCTL_MOTORC_STOP           (IOCTL_USER_START + 0x02) // 停
+#define IOCTL_MOTORC_GOTO_ROUTE     (IOCTL_USER_START + 0x03) // 百分比
+#define IOCTL_MOTORC_SET_UP_ROUTE   (IOCTL_USER_START + 0x04) // 设置上行程
+#define IOCTL_MOTORC_SET_DOWN_ROUTE (IOCTL_USER_START + 0x05) // 设置下行程
+#define IOCTL_MOTORC_CLEAR_ROUTE    (IOCTL_USER_START + 0x06) // 清除行程
+#define IOCTL_MOTORC_TURN           (IOCTL_USER_START + 0x07) // 开停关停
+
+
+#define MOTOR_ROUTE_FREE      0x7fffffff
+#define MOTOR_ROUTE_INC_MAX   (0 + (MOTOR_ROUTE_FREE - 1))
+#define MOTOR_ROUTE_DEC_MAX   (0 - (MOTOR_ROUTE_FREE - 1))
+
+
+#define MFLAG_RESISTANCE_INC  (1<<1) // inc过程中遇阻
+#define MFLAG_RESISTANCE_DEC  (1<<2) // dec过程中遇阻
+#define MFLAG_RESISTANCE_SLOW (1<<3) // 减速遇阻
+#define MFLAG_RESISTANCE_STOP (1<<4) // 堵转遇阻
+
+
+#define MTURN_NORMAL          0x00
+#define MTURN_OPEN_STOP       0x01
+#define MTURN_CLOSE_STOP      0x02
+#define MTURN_OPEN_CLOSE      0x03
+
+#define MSTATE_STOP           0 // 停止
+#define MSTATE_RUN_INC        1 // 正转
+#define MSTATE_RUN_DEC        2 // 反转
+
+#define MMODE_NORMAL          (1 << 0) // 正常
+#define MMODE_SSTEP           (1 << 2) // 点动
+#define MMODE_STOP_IFRUN      (1 << 3) // 如果电机正在运行，则停止，否则执行对应的动作
+#define MMODE_OVERROUTE       (1 << 4) // 允许超出行程
+
+//default
+#define MOTOR_SPACE_MIN       (10)       // 默认允许运行的最小行程
+#define MOTOR_SWITCH_TIME     (500)      // 正反切换延时。单位毫秒
+// // #define MOTOR_PW_MAX          (300 ms) // 允许的最大脉冲宽度（超过此宽度认为遇阻）
+// #define MOTOR_PW_DMAX         0x40     // 允许的脉宽最大的变化系数（超过此值认为遇阻)
+
+
+
+
+// #define preMax                100
+
+// #define JOG_HALL              20  // 点动霍尔数
+// #define MOTOR_ROUTE_MIN       300 // 行程最小值
+
+
+
+// #define MIN_SPEED             20.0f // 最小速度 单位hz
+// #define MAX_SPEED             130.0f // 最大速度 单位hz
+// #define SLOW_SPEED_ROUTE      150    // 慢启慢停所需要的行程 单位：霍尔数
+// #define SLOW_SPEED_D          5     // 变化的速度 单位：hz
+/*---------- type define ----------*/
+/*---------- variable prototype ----------*/
+#pragma pack(1)
+typedef struct {
+    struct {
+        bool (*init)(void);
+        void (*deinit)(void);
+        bool (*save_config)(void); // 保存电机config内的数据到非易失性存储器
+        bool (*load_config)(void); // 从非易失性数据存储器内读取电机的config数据
+        void (*motor_stop)(void); //电机停止
+        void (*motor_inc)(void); //电机正转
+        void (*motor_dec)(void); //电机反转
+    } ops;
+
+    struct {
+        int32_t route_curr; // 当前行程
+        int32_t route_up;   // 上行程
+        int32_t route_down; // 下行程
+    } config;
+
+    struct {
+        struct {
+            uint8_t state_curr;         // 当前状态（逻辑）
+            uint8_t is_running;         // 当前状态（电机）
+            uint8_t last_event;         // 上次状态，用于开停关停
+            uint16_t disable_excloop;   // 开停关停循环之间的死区时间
+            uint16_t start_update_time; // 5秒间隔上报计时
+        } state;
+
+        struct {
+            int32_t target_route;    // 目标行程
+            int32_t route_start;     // 停止时记录行程
+            uint16_t time_stop_delay;// 当正反向切换时，继电器需要的延时时间。单位ms
+            uint8_t is_jog;          // 点动模式
+        } control;
+
+        struct {
+            uint8_t state;      // 遇阻状态(哪个方向遇阻)
+            uint32_t run_time;  // 运行时间(电机运行5分钟需要保护)
+            bool is_resistance; // 已遇阻 (过流或刹车或一切需要电机立即停止的标志位)
+        } flag;
+    } priv;
+
+    struct {
+        uint32_t space_min;
+        uint16_t time_stop_delay;
+    } param;
+} roller_blind_control_describe_t;
+#pragma pack()
+/*---------- function prototype ----------*/
+/*---------- end of file ----------*/
+#ifdef __cplusplus
+}
+#endif
+#endif // !__ROLLER_BLIND_CONTROL_H__
